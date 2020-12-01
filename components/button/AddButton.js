4626fcc7e1842c8b8f78ee067aef7002495f6b46
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-12-01 10:06:19 +0100

    navigation feature for add button

--- components/button/AddButton.js
+++ components/button/AddButton.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 
 const AddButton = () => {
+    const navigation = useNavigation();
     return (
-        <TouchableOpacity activeOpacity={0.8}>
+        <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('AddTodo')}>
             <Icon name='circle-with-plus' size={80} color='red' />
         </TouchableOpacity>
     );
